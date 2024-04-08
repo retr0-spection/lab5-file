d6b46b9d0cee3cbc60612e345623a17b08524438
retr0-spection <ratinailana@icloud.com>
2024-04-08 16:45:24 +0200

    port 80

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ app.use(function (req, res, next) {
 })
 
 //get all cars
-app.get('/cars', (req, res) => {
+app.get('api/cars', (req, res) => {
     res.json(cars);
 });
 
 //get car by id
-app.get('/cars/:id', (req, res) => {
+app.get('api/cars/:id', (req, res) => {
     const id = req.params.id;
     const car = cars.find(car => car.id === id);
     res.json(car);
 });
 
 //update car
-app.put('/cars/:id', (req, res) => {
+app.put('api/cars/:id', (req, res) => {
     const id = req.params.id;
     const updatedCar = req.body;
     const index = cars.findIndex(car => car.id === id);
@@ -35,7 +35,7 @@ app.put('/cars/:id', (req, res) => {
 });
 
 //delete car
-app.delete('/cars/:id', (req, res) => {
+app.delete('api/cars/:id', (req, res) => {
     const id = req.params.id;
     const index = cars.findIndex(car => car.id === id);
     cars.splice(index, 1);
@@ -43,7 +43,7 @@ app.delete('/cars/:id', (req, res) => {
 });
 
 //add car
-app.post('/cars', (req, res) => {
+app.post('api/cars', (req, res) => {
     console.log(req);
     const newCar = req.body;
     console.log(newCar);
